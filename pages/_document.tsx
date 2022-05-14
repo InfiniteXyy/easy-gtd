9fc3229a97810d24875602c0098673dd55b91163
infiniteXyy <xuyiyangwing@gmail.com>
2022-05-14 23:36:49 +0800

    chore: update favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
